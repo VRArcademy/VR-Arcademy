58fc954d1f136b115c0c61eef9bd85e64401d45d
VR_Arcademy <distepstar@gmail.com>
2017-12-04 22:11:14 +0800

    VR_Arcademy_04122017

---
+++ VR_Arcademy_04_12_2017/VR_Arcademy_04_12_2017/Assets/Scripts/GunController.cs
@@ -0,0 +1,31 @@
+﻿namespace VRTK
+{
+using UnityEngine;
+
+public class GunController : VRTK_InteractableObject {
+		public GameObject Emitter;
+		public GameObject BulletPrefab;
+
+		[SerializeField]private float speed = 1000f;
+
+
+		public override void StartUsing(VRTK_InteractUse usingObject){
+
+			base.StartUsing (usingObject);
+			GunFire ();
+		}
+
+		protected void Start(){
+			
+		}
+
+		public void GunFire(){
+			Rigidbody gunRig;
+			GameObject shootBullet = Instantiate (BulletPrefab, Emitter.transform.position, Emitter.transform.rotation);
+			gunRig = shootBullet.GetComponent<Rigidbody> ();
+			VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(usingObject.gameObject), 1.0f );
+			gunRig.AddForce (transform.forward * speed);
+			Destroy (shootBullet, 10.0f);
+		}
+	}
+}
---
+++ VR_Arcademy_04_12_2017/VR_Arcademy_04_12_2017/Assets/Scripts/bulletScripts.cs
@@ -0,0 +1,23 @@
+﻿namespace VRTK
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class bulletScripts : MonoBehaviour {
+
+		void Start () {
+			
+		}
+
+		void Update () {
+			Destroy (gameObject, 3.0f);
+		}
+
+		/*void OnTriggerEnter(Collider other){
+			if (other) {
+				Destroy (this.gameObject);
+			}
+		}*/
+	}
+}
